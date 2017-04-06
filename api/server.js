e95eb790b06f5725dd81d3aee4e63a87a6a40859
Simon Schärer <axax@gmx.net>
2017-04-06 23:08:22 +0200

    Pass dbConnection Object to data resolver

--- api/server.js
+++ api/server.js
@@ -3,29 +3,34 @@ import graphqlHTTP from 'express-graphql'
 
 import {schema} from './schema'
 import {resolver} from './resolver'
+import {dbConnection} from './database'
 
 const PORT = 8080
 
 // Initialize http api
 const app = express()
 
-
-// delay response
-/*app.use(function (req, res, next) {
- setTimeout(next, 1000)
- })*/
-
-app.use('/graphql', graphqlHTTP({
-	schema: schema,
-	rootValue: resolver,
-	graphiql: true,
-	extensions({document, variables, operationName, result}) {
-	}
-}))
+dbConnection((db) => {
 
 
-// Launch the api
-const server = app.listen(PORT, () => {
-	const {port} = server.address()
-	console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`)
+	// delay response
+	/*app.use(function (req, res, next) {
+	 setTimeout(next, 1000)
+	 })*/
+
+	app.use('/graphql', graphqlHTTP({
+		schema: schema,
+		rootValue: resolver(db),
+		graphiql: true,
+		extensions({document, variables, operationName, result}) {
+		}
+	}))
+
+
+	// Launch the api
+	const server = app.listen(PORT, () => {
+		const {port} = server.address()
+		console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`)
+	})
+
 })
