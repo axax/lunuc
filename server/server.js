b63146ae39a9fe27cf0cf731d982431b36de2644
Simon Schärer <axax@gmx.net>
2017-08-05 15:12:38 +0200

    Include proxy in express server for api

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import path from 'path'
+import proxy from 'express-http-proxy'
 
 // Port to listen to
-const port = (process.env.PORT || 8080)
+const PORT = (process.env.PORT || 8080)
+const API_PORT = (process.env.API_PORT || 3000)
 
 // Build dir
-var build_dir = path.join(__dirname, 'build')
+var build_dir = path.join(__dirname, '../build')
 
 
 // Initialize http api
@@ -15,15 +17,26 @@ const app = express()
 //Serving the files on the dist folder
 app.use(express.static(build_dir))
 
+// Proxy for API
+app.use('/graphql', proxy(`localhost:${API_PORT}`, {
+	proxyReqPathResolver: function(req) {
+		return '/graphql'+require('url').parse(req.url).path
+	}
+}))
 
 //Send index.html when the user access the web
-app.get('*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'))
+app.get('/graphql', function (req, res) {
+	console.log('graphql',req)
 })
 
 
+//Send index.html when the user access the web
+app.get('*', function (req, res) {
+	res.sendFile(path.join(build_dir, '/../index.html'))
+})
+
 // Launch the server
-const server = app.listen(port, () => {
+const server = app.listen(PORT, () => {
 	const {address, port} = server.address()
 
 	console.log(`Listening at http://${address}:${port}`)
