a24f5afab1a9c58e0abda3318bbd07b5a813ebf5
Simon Schärer <axax@gmx.net>
2017-10-18 13:21:46 +0200

    Add translation function

--- api/data/common.js
+++ api/data/common.js
@@ -1,2 +1,494 @@
+export const allAvailableLanguages = [
+    {
+        'language': 'Afrikaans',
+        'countryCodes': [
+            {
+                'langCode': 'af-ZA',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Bahasa Indonesia',
+        'countryCodes': [
+            {
+                'langCode': 'id-ID',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Bahasa Melayu',
+        'countryCodes': [
+            {
+                'langCode': 'ms-MY',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Català',
+        'countryCodes': [
+            {
+                'langCode': 'ca-ES',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Čeština',
+        'countryCodes': [
+            {
+                'langCode': 'cs-CZ',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Dansk',
+        'countryCodes': [
+            {
+                'langCode': 'da-DK',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Deutsch',
+        'countryCodes': [
+            {
+                'langCode': 'de-DE',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'English',
+        'countryCodes': [
+            {
+                'langCode': 'en-AU',
+                'country': 'Australia'
+            },
+            {
+                'langCode': 'en-CA',
+                'country': 'Canada'
+            },
+            {
+                'langCode': 'en-IN',
+                'country': 'India'
+            },
+            {
+                'langCode': 'en-NZ',
+                'country': 'New Zealand'
+            },
+            {
+                'langCode': 'en-ZA',
+                'country': 'South Africa'
+            },
+            {
+                'langCode': 'en-GB',
+                'country': 'United Kingdom'
+            },
+            {
+                'langCode': 'en-US',
+                'country': 'United States'
+            }
+        ]
+    },
+    {
+        'language': 'Español',
+        'countryCodes': [
+            {
+                'langCode': 'es-AR',
+                'country': 'Argentina'
+            },
+            {
+                'langCode': 'es-BO',
+                'country': 'Bolivia'
+            },
+            {
+                'langCode': 'es-CL',
+                'country': 'Chile'
+            },
+            {
+                'langCode': 'es-CO',
+                'country': 'Colombia'
+            },
+            {
+                'langCode': 'es-CR',
+                'country': 'Costa Rica'
+            },
+            {
+                'langCode': 'es-EC',
+                'country': 'Ecuador'
+            },
+            {
+                'langCode': 'es-SV',
+                'country': 'El Salvador'
+            },
+            {
+                'langCode': 'es-ES',
+                'country': 'España'
+            },
+            {
+                'langCode': 'es-US',
+                'country': 'Estados Unidos'
+            },
+            {
+                'langCode': 'es-GT',
+                'country': 'Guatemala'
+            },
+            {
+                'langCode': 'es-HN',
+                'country': 'Honduras'
+            },
+            {
+                'langCode': 'es-MX',
+                'country': 'México'
+            },
+            {
+                'langCode': 'es-NI',
+                'country': 'Nicaragua'
+            },
+            {
+                'langCode': 'es-PA',
+                'country': 'Panamá'
+            },
+            {
+                'langCode': 'es-PY',
+                'country': 'Paraguay'
+            },
+            {
+                'langCode': 'es-PE',
+                'country': 'Perú'
+            },
+            {
+                'langCode': 'es-PR',
+                'country': 'Puerto Rico'
+            },
+            {
+                'langCode': 'es-DO',
+                'country': 'República Dominicana'
+            },
+            {
+                'langCode': 'es-UY',
+                'country': 'Uruguay'
+            },
+            {
+                'langCode': 'es-VE',
+                'country': 'Venezuela'
+            }
+        ]
+    },
+    {
+        'language': 'Euskara',
+        'countryCodes': [
+            {
+                'langCode': 'eu-ES',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Filipino',
+        'countryCodes': [
+            {
+                'langCode': 'fil-PH',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Français',
+        'countryCodes': [
+            {
+                'langCode': 'fr-FR',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Galego',
+        'countryCodes': [
+            {
+                'langCode': 'gl-ES',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Hrvatski',
+        'countryCodes': [
+            {
+                'langCode': 'hr_HR',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'IsiZulu',
+        'countryCodes': [
+            {
+                'langCode': 'zu-ZA',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Íslenska',
+        'countryCodes': [
+            {
+                'langCode': 'is-IS',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Italiano',
+        'countryCodes': [
+            {
+                'langCode': 'it-IT',
+                'country': 'Italia'
+            },
+            {
+                'langCode': 'it-CH',
+                'country': 'Svizzera'
+            }
+        ]
+    },
+    {
+        'language': 'Lietuvių',
+        'countryCodes': [
+            {
+                'langCode': 'lt-LT',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Magyar',
+        'countryCodes': [
+            {
+                'langCode': 'hu-HU',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Nederlands',
+        'countryCodes': [
+            {
+                'langCode': 'nl-NL',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Norsk bokmål',
+        'countryCodes': [
+            {
+                'langCode': 'nb-NO',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Polski',
+        'countryCodes': [
+            {
+                'langCode': 'pl-PL',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Português',
+        'countryCodes': [
+            {
+                'langCode': 'pt-BR',
+                'country': 'Brasil'
+            },
+            {
+                'langCode': 'pt-PT',
+                'country': 'Portugal'
+            }
+        ]
+    },
+    {
+        'language': 'Română',
+        'countryCodes': [
+            {
+                'langCode': 'ro-RO',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Slovenščina',
+        'countryCodes': [
+            {
+                'langCode': 'sl-SI',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Slovenčina',
+        'countryCodes': [
+            {
+                'langCode': 'sk-SK',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Suomi',
+        'countryCodes': [
+            {
+                'langCode': 'fi-FI',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Svenska',
+        'countryCodes': [
+            {
+                'langCode': 'sv-SE',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Tiếng Việt',
+        'countryCodes': [
+            {
+                'langCode': 'vi-VN',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Türkçe',
+        'countryCodes': [
+            {
+                'langCode': 'tr-TR',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Ελληνικά',
+        'countryCodes': [
+            {
+                'langCode': 'el-GR',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'български',
+        'countryCodes': [
+            {
+                'langCode': 'bg-BG',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Pусский',
+        'countryCodes': [
+            {
+                'langCode': 'ru-RU',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Српски',
+        'countryCodes': [
+            {
+                'langCode': 'sr-RS',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'Українська',
+        'countryCodes': [
+            {
+                'langCode': 'uk-UA',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': '한국어',
+        'countryCodes': [
+            {
+                'langCode': 'ko-KR',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': '中文',
+        'countryCodes': [
+            {
+                'langCode': 'cmn-Hans-CN',
+                'country': '普通话 (中国大陆)'
+            },
+            {
+                'langCode': 'cmn-Hans-HK',
+                'country': '普通话 (香港)'
+            },
+            {
+                'langCode': 'cmn-Hant-TW',
+                'country': '中文 (台灣)'
+            },
+            {
+                'langCode': 'yue-Hant-HK',
+                'country': '粵語 (香港)'
+            }
+        ]
+    },
+    {
+        'language': '日本語',
+        'countryCodes': [
+            {
+                'langCode': 'ja-JP',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'हिन्दी',
+        'countryCodes': [
+            {
+                'langCode': 'hi-IN',
+                'country': 'Default'
+            }
+        ]
+    },
+    {
+        'language': 'ภาษาไทย',
+        'countryCodes': [
+            {
+                'langCode': 'th-TH',
+                'country': 'Default'
+            }
+        ]
+    }
+]
 
-export const speechLanguages = [{key:'en-US',name:'English'},{key:'de-DE',name:'German'}]
+
+export const speechLanguages = []
+allAvailableLanguages.forEach(function(item){
+    if(item.countryCodes.length > 1){
+        item.countryCodes.forEach(function(countryItem){
+            speechLanguages.push({key:countryItem.langCode,name:item.language + ' ('+countryItem.country+')'})
+        })
+    }else{
+        speechLanguages.push({key:item.countryCodes[0].langCode,name:item.language})
+    }
+})
---
+++ src/containers/SearchWhileSpeechContainer.js
@@ -0,0 +1,183 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {withApollo} from 'react-apollo'
+import ApolloClient from 'apollo-client'
+import {gql, graphql, compose} from 'react-apollo'
+import {connect} from 'react-redux'
+
+
+class SearchWhileSpeechContainer extends React.Component {
+
+    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)()
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            recording: true,
+            recorded: [],
+            search: '',
+            language: 'de-DE',
+            data: []
+        }
+    }
+
+    componentDidMount() {
+        this.createRecorder()
+    }
+
+    componentWillUnmount() {
+        this.recognition.abort()
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.me.settings.speechLang.selection && nextProps.me.settings.speechLang.selection.key !== this.state.language) {
+            this.setState({language: nextProps.me.settings.speechLang.selection.key})
+        }
+    }
+
+    createRecorder = () => {
+        const self = this
+        this.recognition.lang = this.state.language
+        this.recognition.interimResults = false
+        this.recognition.maxAlternatives = 1
+        this.recognition.continuous = false
+        this.recognition.recognizing = false // this is a custom flag to determin wheater recognition is running
+
+
+        this.recognition.onstart = function () {
+            this.recognizing = true
+        }
+
+        this.recognition.onerror = function (event) {
+            this.recognizing = false
+        }
+
+        this.recognition.onresult = function (event) {
+
+            const results = event.results
+            console.log(results)
+
+            for (const result of results) {
+                if (result.isFinal) {
+
+                    for (const alternativ of result) {
+                        console.log(alternativ)
+                        if (alternativ.confidence > 0.60) {
+                            self.setState((state) => ({recorded: state.recorded.concat(alternativ.transcript)}))
+                        }
+                    }
+                }
+            }
+        }
+        this.recognition.onend = function (e) {
+            this.recognizing = false
+            console.log('end')
+            self.handleRecorder(self.state.recording)
+        }
+        this.handleRecorder(this.state.recording)
+    }
+
+
+    handleRecorder = (start) => {
+        if (start) {
+            if (!this.recognition.recognizing) {
+                this.recognition.start()
+            }
+        } else {
+            this.recognition.stop()
+            this.recognition.abort()
+        }
+    }
+
+
+    handleInputChange = (e) => {
+        const target = e.target
+        const value = target.type === 'checkbox' ? target.checked : target.value
+        const name = target.name
+        this.setState({
+            [target.name]: value
+        })
+
+        if (target.type === 'checkbox') {
+            this.handleRecorder(value)
+        } else if (name === 'language') {
+
+            this.props.updateMe({speechLang: value}).then(() => {
+                console.log('change language to', value)
+
+                this.recognition.lang = value
+            })
+        } else if (name === 'search') {
+            this.setState((state) => ({recorded: state.recorded.concat(value)}))
+
+        }
+    }
+
+
+    render() {
+        if (!this.props.me)
+            return null
+
+        const langs = this.props.me.settings.speechLang.data
+
+        let pairs = []
+
+        this.state.recorded.forEach(
+            (k, i) => pairs.push(<p key={i}>{k}</p>)
+        )
+
+        console.log('render', this.state)
+        return <div><h1>Search</h1><input type="text" name="search" value={this.state.search}
+                                          onChange={this.handleInputChange}/>
+            <select disabled={!this.state.recording} name="language" value={this.state.language}
+                    onChange={this.handleInputChange}>
+                {langs.map((lang, i) => {
+                    return <option key={i} value={lang.key}>{lang.name}</option>
+                })}
+            </select>
+            <input
+                name="recording"
+                type="checkbox"
+                checked={this.state.recording}
+                onChange={this.handleInputChange}/>
+            Voice Recorder: {this.state.recording ? 'on' : 'off'}{pairs}
+        </div>
+    }
+}
+
+
+SearchWhileSpeechContainer.propTypes = {
+    client: PropTypes.instanceOf(ApolloClient).isRequired,
+    me: PropTypes.object,
+    updateMe: PropTypes.func.isRequired
+}
+
+
+const SearchWhileSpeechContainerWithGql = compose(
+    graphql(gql`query {me{_id settings{speechLang{selection{key name}data{key name}}}}}`, {
+        options() {
+            return {
+                fetchPolicy: 'cache-and-network'
+            }
+        },
+        props: ({data: {loading, me}}) => ({
+            me,
+            loading
+        })
+    }),
+    graphql(gql`mutation updateMe($speechLang: String!){updateMe(settings: {speechLang:$speechLang}){_id settings{speechLang{selection{key name}}}}}`, {
+        props: ({ownProps, mutate}) => ({
+            updateMe: ({speechLang}) => {
+                return mutate({
+                    variables: {speechLang}
+                })
+            }
+        })
+    })
+)(SearchWhileSpeechContainer)
+
+
+const SearchWhileSpeechContainerWithApollo = withApollo(SearchWhileSpeechContainerWithGql)
+
+
+export default SearchWhileSpeechContainerWithApollo
