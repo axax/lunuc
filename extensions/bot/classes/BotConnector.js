c37bd0e371fba6c389d15e6770d0b419b6010634
Simon Schärer <axax@gmx.net>
2019-05-03 09:11:41 +0200

    Small improvments

--- extensions/bot/classes/BotConnector.js
+++ extensions/bot/classes/BotConnector.js
@@ -31,6 +31,16 @@ class BotConnector {
         return {message_id: this.messageCount}
     }
 
+    replyWithPhoto(data){
+        this.messageCount++
+        if (this._ons['text']) {
+            this._ons['text'].forEach(cb => {
+                cb(`<img src="${data.url}" />`, this.messageCount)
+            })
+        }
+        return {message_id: this.messageCount}
+    }
+
     deleteMessage(id) {
         if (this._ons['deleteMessage']) {
             this._ons['deleteMessage'].forEach(cb => {
