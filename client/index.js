5ad475100408701974a4466237fa484e3c5f9f85
Simon Schärer <axax@gmx.net>
2019-10-06 21:11:20 +0200

    Small improvements

--- client/index.js
+++ client/index.js
@@ -13,45 +13,56 @@ function mainInit() {
 
     const {store} = configureStore()
 
+    // override config
+    if (_app_.languages) {
+        config.LANGUAGES = _app_.languages
+    }
+
     // add config to the global app object
     _app_.config = config
 
-    // context language
-    // we expect the first part of the path to be the language when its length is 2
-    let loc = window.location, contextLanguage = loc.pathname.split('/')[1], basePath
-    if (contextLanguage && contextLanguage.length === 2) {
-        contextLanguage = contextLanguage.toLowerCase()
-        _app_.contextPath = '/' + contextLanguage
-        basePath = loc.pathname.substring(3)
-    } else {
-        _app_.contextPath = ''
-        contextLanguage = false
-        basePath = loc.pathname
-    }
-    basePath += loc.search + loc.hash
+    let contextLanguage, loc = window.location, basePath
 
+    // if multi languages
+    if (config.LANGUAGES && config.LANGUAGES.length > 1) {
 
-    // if lang is not set already
-    if (!_app_.lang) {
-        let lang
-        const sessionLanguage = sessionStorage.getItem('lang')
-        if (contextLanguage) {
-            lang = contextLanguage
+        // context language
+        // we expect the first part of the path to be the language when its length is 2
+        contextLanguage = loc.pathname.split('/')[1]
+        if (contextLanguage && contextLanguage.length === 2) {
+            contextLanguage = contextLanguage.toLowerCase()
+            _app_.contextPath = '/' + contextLanguage
+            basePath = loc.pathname.substring(3)
         } else {
-            if (!sessionLanguage && _app_.detectLang) {
-                lang = (navigator.language || navigator.userLanguage).substr(0, 2)
-            } else {
-                lang = config.DEFAULT_LANGUAGE
-            }
+            _app_.contextPath = ''
+            contextLanguage = false
+            basePath = loc.pathname
         }
-        _app_.langBefore = sessionLanguage
-        _app_.lang = lang
-    }
+        basePath += loc.search + loc.hash
 
-    if (!contextLanguage && config.DEFAULT_LANGUAGE !== _app_.lang) {
-        // add language to url and redirect
-        window.location = loc.origin + '/' + _app_.lang + basePath
-    } else {
+
+        // if lang is not set already
+        if (!_app_.lang) {
+            let lang
+            const sessionLanguage = sessionStorage.getItem('lang')
+            if (contextLanguage) {
+                lang = contextLanguage
+            } else {
+                if (!sessionLanguage && _app_.detectLang) {
+                    lang = (navigator.language || navigator.userLanguage).substr(0, 2)
+                } else {
+                    lang = config.DEFAULT_LANGUAGE
+                }
+            }
+            _app_.langBefore = sessionLanguage
+            _app_.lang = lang
+        }
+        if (!contextLanguage && config.DEFAULT_LANGUAGE !== _app_.lang) {
+            // add language to url and redirect
+            window.location = loc.origin + '/' + _app_.lang + basePath
+            return
+        }
+
         // keep language in session
         sessionStorage.setItem('lang', _app_.lang)
 
@@ -80,54 +91,56 @@ function mainInit() {
                 href: loc.origin + (!isDefault ? '/' + curLang : '') + basePath
             })
         }
+    } else {
+        _app_.contextPath = ''
+    }
 
 
-        const start = () => {
-            render(
-                <App store={store}/>,
-                appEl
-            )
-        }
+    const start = () => {
+        render(
+            <App store={store}/>,
+            appEl
+        )
+    }
 
-        // make sure translations are loaded before start rendering
-        if (_app_.trLoaded) {
-            start()
-        } else {
-            // trCallback gets called as soon as translations are loaded
-            _app_.trCallback = start
-        }
+    // make sure translations are loaded before start rendering
+    if (_app_.trLoaded) {
+        start()
+    } else {
+        // trCallback gets called as soon as translations are loaded
+        _app_.trCallback = start
+    }
 
 
-        /* Register serviceworker only on production. only works with https */
-        if ('serviceWorker' in navigator) {
-            console.log('Service Worker is supported')
+    /* Register serviceworker only on production. only works with https */
+    if ('serviceWorker' in navigator) {
+        console.log('Service Worker is supported')
 
-            if ('PushManager' in window) {
-                console.log('Push is supported')
+        if ('PushManager' in window) {
+            console.log('Push is supported')
 
-                if (config.DEV_MODE) {
-                    navigator.serviceWorker.getRegistrations().then(function (registrations) {
-                        for (let registration of registrations) {
-                            registration.unregister()
-                        }
-                    })
-                } else {
-                    navigator.serviceWorker.register('/serviceworker.js')
-                        .then(function (swReg) {
-                            console.log('Service Worker is registered', swReg)
-                        })
-                        .catch(function (error) {
-                            console.error('Service Worker Error', error)
-                        })
-                }
+            if (config.DEV_MODE) {
+                navigator.serviceWorker.getRegistrations().then(function (registrations) {
+                    for (let registration of registrations) {
+                        registration.unregister()
+                    }
+                })
             } else {
-                console.warn('Push is not supported')
+                navigator.serviceWorker.register('/serviceworker.js')
+                    .then(function (swReg) {
+                        console.log('Service Worker is registered', swReg)
+                    })
+                    .catch(function (error) {
+                        console.error('Service Worker Error', error)
+                    })
             }
-
-
         } else {
-            console.warn('Service Worker is not supported')
+            console.warn('Push is not supported')
         }
+
+
+    } else {
+        console.warn('Service Worker is not supported')
     }
 }
 
