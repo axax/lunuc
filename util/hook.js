6de3f2b409bd5caad304fed6fa3053f524ec18a6
Simon Schärer <axax@gmx.net>
2019-12-07 19:13:17 +0100

    Small improvements

--- util/hook.js
+++ util/hook.js
@@ -19,30 +19,32 @@ const Hook = {
         }
         names.forEach(nameWithKey => {
             const parts = nameWithKey.split('.'),
-                name=parts[0],
-                key=parts.length>1?parts[1]:undefined
+                name = parts[0],
+                key = parts.length > 1 ? parts[1] : undefined
 
             if ('undefined' == typeof (Hook.hooks[name])) {
                 Hook.hooks[name] = []
             }
+
+            if (key) {
+                for (let i = Hook.hooks[name].length - 1; i >= 0; i--) {
+                    if (Hook.hooks[name][i].key === key) {
+                        console.log(`remove hook ${name} with key ${key}`)
+                        Hook.hooks[name].splice(i,1)
+                    }
+                }
+            }
+
             if (!order) {
                 order = 1
             }
 
+
             for (let i = 0; i < Hook.hooks[name].length; i++) {
                 const hook = Hook.hooks[name][i]
                 if (hook.order > order) {
-                    Hook.hooks[name].splice(i, 0, {callback, order})
+                    Hook.hooks[name].splice(i, 0, {callback, order, key})
                     return
-                }
-            }
-            if( key ){
-                for(let i=0; i<Hook.hooks[name].length; i++)
-                {
-                    if(Hook.hooks[name][i].key===key) {
-                        console.log(`remove hook ${name} with key ${key}`)
-                        Hook.hooks[name].splice(i)
-                    }
                 }
             }
             Hook.hooks[name].push({callback, order, key})
