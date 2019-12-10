9bf3665675a8c411d2b470fb3d07b7878a38c62a
Simon Schärer <axax@gmx.net>
2019-12-10 20:14:38 +0100

    Small improvements

--- extensions/cms/components/elementWatcher.js
+++ extensions/cms/components/elementWatcher.js
@@ -4,7 +4,10 @@ export default function elementWatcher({jsonDom, key, eleType, tagName, eleProps
     // ...and returns another component...
     return class extends React.Component {
 
-        state = {madeVisible: false, initialVisible: tagName === 'SmartImage' ? false : (options.initialClass && !options.waitVisible) || !!options.waitVisible}
+        state = {
+            madeVisible: false,
+            initialVisible: tagName === 'SmartImage' ? false : (options.initialClass && !options.waitVisible) || !!options.waitVisible
+        }
 
         constructor(props) {
             super(props)
@@ -19,9 +22,9 @@ export default function elementWatcher({jsonDom, key, eleType, tagName, eleProps
         render() {
             const {initialVisible, madeVisible} = this.state
             if (!initialVisible && !madeVisible) {
-                return <div _key={key} data-wait-visible={jsonDom.instanceId}>...</div>
+                return <div _key={key} data-wait-visible={jsonDom.instanceId} {...eleProps}></div>
             } else {
-                if(!eleProps.className){
+                if (!eleProps.className) {
                     eleProps.className = ''
                 }
                 if (madeVisible && options.visibleClass) {
@@ -40,7 +43,6 @@ export default function elementWatcher({jsonDom, key, eleType, tagName, eleProps
 
         addIntersectionObserver() {
             const ele = document.querySelector(`[_key='${key}']`)
-            console.log(ele, key)
             if (ele) {
                 let observer = new IntersectionObserver((entries, observer) => {
                     entries.forEach(entry => {
@@ -49,12 +51,29 @@ export default function elementWatcher({jsonDom, key, eleType, tagName, eleProps
                             if (this.state.initialVisible) {
                                 ele.classList.add(options.visibleClass)
                             } else {
-                                this.setState({madeVisible: true})
+                                if (tagName === 'SmartImage') {
+                                    const img = new Image()
+                                    img.src = eleProps.src
+
+                                    const timeout = setTimeout(()=>{
+                                        // gifs can be show even if they are not fully loaded
+                                        img.onerror = img.onload = null
+                                        this.setState({madeVisible: true})
+                                    },1000)
+
+                                    img.onerror = img.onload = () => {
+                                        clearTimeout(timeout)
+                                        this.setState({madeVisible: true})
+                                    }
+
+                                } else {
+                                    this.setState({madeVisible: true})
+                                }
                             }
 
                         }
                     })
-                }, {rootMargin: '-100px -100px -100px -100px'})
+                }, {rootMargin: '10%'})
                 observer.observe(ele)
             }
         }
