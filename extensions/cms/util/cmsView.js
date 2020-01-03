ee963b66a90601bf9cde21f1ca459ca559753115
Simon Schärer <axax@gmx.net>
2020-01-04 00:38:09 +0100

    Small improvements

--- extensions/cms/util/cmsView.js
+++ extensions/cms/util/cmsView.js
@@ -13,7 +13,7 @@ export const settingKeyPrefix = 'CmsViewContainerSettings'
 let _gqlQuery
 export const gqlQuery = ()=>{
     if(!_gqlQuery) {
-        _gqlQuery = gql`query cmsPage($slug:String!,$query:String,$props:String,$nosession:String,$editmode:Boolean,$_version:String){cmsPage(slug:$slug,query:$query,props:$props,nosession:$nosession,editmode:$editmode,_version:$_version){cacheKey slug name{${_app_.lang}} urlSensitiv template script serverScript resources dataResolver ssr public online resolvedData parseResolvedData alwaysLoadAssets html subscriptions _id modifiedAt createdBy{_id username} status}}`
+        _gqlQuery = gql`query cmsPage($slug:String!,$query:String,$props:String,$nosession:String,$editmode:Boolean,$_version:String){cmsPage(slug:$slug,query:$query,props:$props,nosession:$nosession,editmode:$editmode,_version:$_version){cacheKey slug name{en de} urlSensitiv template script serverScript resources dataResolver ssr public online resolvedData parseResolvedData alwaysLoadAssets html subscriptions _id modifiedAt createdBy{_id username} status}}`
     }
     return _gqlQuery
 }
