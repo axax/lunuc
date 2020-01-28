2c8bd102d812997a1b9d2ef0d28f958ff2dbe7f9
Simon Schärer <axax@gmx.net>
2020-01-28 10:31:28 +0100

    Small improvements

--- client/index.js
+++ client/index.js
@@ -8,6 +8,16 @@ import configureStore from './store/index'
 import config from 'gen/config'
 import DomUtil from 'client/util/dom'
 
+if (typeof localStorage === 'object') {
+    try {
+        localStorage.setItem('localStorage', 1)
+        localStorage.removeItem('localStorage')
+    } catch (e) {
+        Storage.prototype.setItem = function() {}
+        console.log('Your web browser does not support storing settings locally. In Safari, the most common cause of this is using "Private Browsing Mode". Some settings may not save or some features may not work properly for you.')
+    }
+}
+
 function mainInit() {
     const {store} = configureStore()
 
