03eeddcfe6e7d8dbecfe2b38f2dc6afb947891e9
Simon Schärer <axax@gmx.net>
2020-02-18 16:58:17 +0100

    Small improvements

--- extensions/usertracking/track.js
+++ extensions/usertracking/track.js
@@ -6,7 +6,7 @@ export const trackUser = async ({req, event, slug, db, context})=>{
 
     const ip = clientAddress(req)
 
-    if( ip && ip !== '::ffff:127.0.0.1' && ip !== '144.91.119.30' ) {
+    if( ip && ip !== '::ffff:127.0.0.1' && ip !== '::ffff:144.91.119.30' ) {
         const host = getHostFromHeaders(req.headers)
 
         const data = {
