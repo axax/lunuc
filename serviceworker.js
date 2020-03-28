02109638528715b7bb97e7493bbd34494507af80
Simon Schärer <axax@gmx.net>
2020-03-28 10:04:36 +0100

    Small improvements

--- serviceworker.js
+++ serviceworker.js
@@ -2,12 +2,13 @@
 // Change to vxxx, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
 const PRECACHE = 'precache-v${BUILD_NUMBER}'
-const RUNTIME = 'runtime'
+const RUNTIME = 'runtime-v${BUILD_NUMBER}'
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
-    /*'./',*/ // Alias for index.html
-    /*'main.bundle.js?v=${BUILD_NUMBER}'*/
+    '/', // Alias for index.html
+    'main.bundle.js?v=${BUILD_NUMBER}',
+    'manifest.json?v=${BUILD_NUMBER}'
 ]
 
 // a list of cross origin domains that we want to cache
@@ -24,21 +25,25 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open(PRECACHE)
             .then(cache => cache.addAll(PRECACHE_URLS))
-            .then(self.skipWaiting())
+            /*.then(self.skipWaiting())*/
     )
 })
 
-// The activate handler takes care of cleaning up old caches.
+
 self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE, RUNTIME];
+    const expectedCaches = [PRECACHE, RUNTIME];
+
+    // delete any caches that aren't in expectedCaches
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName))
-        }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }))
-        }).then(() => self.clients.claim())
+        caches.keys().then(keys => Promise.all(
+            keys.map(key => {
+                if (!expectedCaches.includes(key)) {
+                    return caches.delete(key)
+                }
+            })
+        )).then(() => {
+            console.log('${BUILD_NUMBER} now ready to handle fetches!')
+        })
     )
 })
 
@@ -59,30 +64,19 @@ self.addEventListener('fetch', event => {
     if (event.request.method == 'GET' && (event.request.url.startsWith(self.location.origin) ||
             HOSTS.some((host) => event.request.url.startsWith(host))
         )) {
+
+
         event.respondWith(
-            caches.match(event.request).then(cachedResponse => {
-                if (cachedResponse) {
-                    requestAndCache(event)
-                    return cachedResponse
-                }
-                return requestAndCache(event)
+            caches.match(event.request).then((resp) => {
+                return resp || fetch(event.request).then((response) => {
+                    let responseClone = response.clone()
+                    caches.open(RUNTIME).then((cache) => {
+                        cache.put(event.request, responseClone)
+                    })
+
+                    return response
+                })
             })
         )
     }
 })
-
-function requestAndCache(event) {
-
-    return caches.open(RUNTIME).then(cache => {
-        return fetch(event.request).then(response => {
-            if (response.status === 200) {
-                // Put a copy of the response in the runtime cache.
-                return cache.put(event.request, response.clone()).then(() => {
-                    return response
-                })
-            } else {
-                return response
-            }
-        })
-    })
-}
