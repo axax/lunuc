b3db342f8cb2a42dc25cc628a29d1c8399827e19
Simon Schärer <axax@gmx.net>
2020-04-01 23:12:31 +0200

    Small improvements

--- server/server.js
+++ server/server.js
@@ -94,7 +94,7 @@ const app = httpx.createServer(options, function (req, res) {
         if (!config.DEV_MODE && this.constructor.name === 'Server') {
             if (process.env.LUNUC_FORCE_HTTPS) {
 
-                const agent = req.headers['user-agent'], agentParts = agent.split(' ')
+                const agent = req.headers['user-agent'], agentParts = agent?agent.split(' '):[]
 
                 let browser, version
                 if (agentParts.length > 2) {
