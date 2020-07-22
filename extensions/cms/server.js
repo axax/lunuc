41f1face31da19b05dfcbdc94c3cb16d0a1b0121
Simon Schärer <axax@gmx.net>
2020-07-22 10:39:26 +0200

    Small improvements

--- extensions/cms/server.js
+++ extensions/cms/server.js
@@ -65,7 +65,7 @@ Hook.on('cmsTemplateRenderer', async ({db, context, body, slug}) => {
         throw new Error(`Error in body: ${e.message}`)
         mailContext = {}
     }
-    const scope = {context: mailContext, page: {slug}}
+    const scope = {page: {slug}}
     const {template, script, dataResolver} = cmsPages.results[0]
     const {resolvedData} = await resolveData({db, context, dataResolver, scope})
 
@@ -94,10 +94,10 @@ Hook.on('cmsTemplateRenderer', async ({db, context, body, slug}) => {
                              script={script}
                              location={loc}
                              history={{location:loc}}
-                             slug="_ssr"
+                             slug={slug}
+                             _props={{context: mailContext}}
                              resolvedData={JSON.stringify(resolvedData)}
-                             editMode={false}
-                             scope={JSON.stringify(scope)}/>
+                             editMode={false}/>
                 </ApolloProvider>
             </Provider>
         </UIProvider>)
