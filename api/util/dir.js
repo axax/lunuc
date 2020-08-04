21ae7c0c3c1d2abb98e5a4ea4a936990df21000a
Simon Schärer <axax@gmx.net>
2020-08-04 11:14:49 +0200

    Small improvements

--- api/util/dir.js
+++ api/util/dir.js
@@ -1,12 +1,14 @@
 import fs from 'fs'
 
-export const rmDir = (dirPath, keepDir)=> {
+export const rmDir = (dirPath, keepDir, options = {})=> {
     try { var files = fs.readdirSync(dirPath) }
     catch(e) { return }
     if (files.length > 0)
         for (var i = 0; i < files.length; i++) {
-            var filePath = dirPath + '/' + files[i]
-            console.log('remove file '+filePath)
+            const filePath = dirPath + '/' + files[i]
+            if(options.log) {
+                console.log('remove file '+filePath)
+            }
             if (fs.statSync(filePath).isFile())
                 fs.unlinkSync(filePath)
             else
