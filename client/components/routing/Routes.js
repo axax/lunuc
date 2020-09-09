2ac83dfb4aacd81b545f4a385f9548e554fd41f2
Simon Schärer <axax@gmx.net>
2020-09-09 16:42:54 +0200

    Small improvements

--- client/components/routing/Routes.js
+++ client/components/routing/Routes.js
@@ -10,7 +10,7 @@ const {ADMIN_BASE_URL} = config
 import LogoutContainer from 'client/containers/LogoutContainer'
 import Async from 'client/components/Async'
 import {
-    CAPABILITY_ACCESS_ADMIN_PAGE, CAPABILITY_MANAGE_TYPES
+    CAPABILITY_ACCESS_ADMIN_PAGE
 } from 'util/capabilities'
 
 const LoginContainer = (props) => <Async {...props}
@@ -46,6 +46,7 @@ class Routes extends React.Component {
         {path: ADMIN_BASE_URL + '/login', component: LoginContainer},
         {path: ADMIN_BASE_URL + '/logout', component: LogoutContainer},
         {path: ADMIN_BASE_URL + '/signup', component: SignUpContainer},
+        {exact: true, private: true, path: ADMIN_BASE_URL + '/types/:type*', component: TypesContainer}
         {private: true, path: ADMIN_BASE_URL + '/profile', component: UserProfileContainer},
         {private: true, path: ADMIN_BASE_URL + '/system', component: SystemContainer},
         {private: true, path: ADMIN_BASE_URL + '/backup', component: DbDumpContainer},
@@ -54,13 +55,6 @@ class Routes extends React.Component {
 
     constructor(props) {
         super(props)
-
-        const {user: {userData}} = this.props
-        const capabilities = (userData && userData.role && userData.role.capabilities) || []
-
-        if(capabilities.indexOf(CAPABILITY_MANAGE_TYPES) >= 0){
-            this.routes.push({exact: true, private: true, path: ADMIN_BASE_URL + '/types/:type*', component: TypesContainer})
-        }
 
         Hook.call('Routes', {routes: this.routes, container: this})
 
