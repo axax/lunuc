211a28bc6caf0a9fd02b3a02b6bcecb390783c0f
Simon Schärer <axax@gmx.net>
2021-06-14 23:25:24 +0200

    Small improvements

---
+++ extensions/genericdata/translations/admin.js
@@ -0,0 +1,8 @@
+export const translations = {
+    de: {
+        'GenericData.createNewHint': 'Bitte wählen Sie einen generischen Typ aus, den Sie erstellen möchten, und drücken Sie auf "Speichern".'
+    },
+    en: {
+        'GenericData.createNewHint': 'Please select a generic type you want to create and press save.'
+    }
+}
---
+++ extensions/post/client-admin.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Hook from 'util/hook'
+import config from 'gen/config-client'
+
+const {ADMIN_BASE_URL} = config
+import Async from 'client/components/Async'
+import Util from '../../client/util'
+import {CAPABILITY_MANAGE_CMS_TEMPLATE} from '../cms/constants'
+
+const PostRenderer = (props) => <Async readOnly={true} {...props}
+                                       load={import(/* webpackChunkName: "post" */ './components/post/PostEditor')}/>
+const EditIcon = (props) => <Async {...props} expose="EditIcon"
+                                   load={import(/* webpackChunkName: "admin" */ '../../gensrc/ui/admin')}/>
+
+export default () => {
+
+    // add entry to main menu
+    Hook.on('MenuMenu', ({menuItems, user}) => {
+        if(Util.hasCapability(user, CAPABILITY_MANAGE_CMS_TEMPLATE)) {
+            menuItems.push({name: 'Posts', to: ADMIN_BASE_URL + '/post', auth: true, icon: <EditIcon/>})
+        }
+    })
+
+
+    Hook.on('GenericFormField', function ({field, value, result}) {
+        if (field.uitype === 'richtext') {
+
+            result.component = <PostRenderer
+                key={field.name}
+                onBlur={this.handleBlur}
+                onChange={(e) => {
+                    console.log(e, e.constructor)
+                    this.handleInputChange({target: {name: field.name, value: e}})
+                }}
+                readOnly={false}
+                imageUpload={false}
+                post={{body:value}}/>
+        }
+    })
+}
