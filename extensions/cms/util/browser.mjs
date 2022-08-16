4239a823d22975ece748cd6003d22c71ece23240
Simon Schärer <axax@gmx.net>
2022-08-16 17:17:15 +0200

    Improve browser dataResolver
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- extensions/cms/util/browser.mjs
+++ extensions/cms/util/browser.mjs
@@ -76,11 +76,52 @@ async function processWebsiteData(processData) {
 
 const openInBrowser = async (options, scope, resolvedData) => {
 
-    const {url, pipeline, images, ignoreSsl, waitUntil, timeout, viewPort, evaluateOnNewDocument} = options
+    const {url, pipeline, args, images, ignoreSsl, waitUntil, timeout, viewPort, evaluateOnNewDocument} = options
     let data = {}, error
+
+
+    const minimal_args = [
+        '--autoplay-policy=user-gesture-required',
+        '--disable-background-networking',
+        '--disable-background-timer-throttling',
+        '--disable-backgrounding-occluded-windows',
+        '--disable-breakpad',
+        '--disable-client-side-phishing-detection',
+        '--disable-component-update',
+        '--disable-default-apps',
+        '--disable-dev-shm-usage',
+        '--disable-domain-reliability',
+        '--disable-extensions',
+        '--disable-features=AudioServiceOutOfProcess',
+        '--disable-hang-monitor',
+        '--disable-ipc-flooding-protection',
+        '--disable-notifications',
+        '--disable-offer-store-unmasked-wallet-cards',
+        '--disable-popup-blocking',
+        '--disable-print-preview',
+        '--disable-prompt-on-repost',
+        '--disable-renderer-backgrounding',
+        '--disable-setuid-sandbox',
+        '--disable-speech-api',
+        '--disable-sync',
+        '--hide-scrollbars',
+        '--ignore-gpu-blacklist',
+        '--metrics-recording-only',
+        '--mute-audio',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--no-pings',
+        '--no-sandbox',
+        '--no-zygote',
+        '--password-store=basic',
+        '--use-gl=swiftshader',
+        '--use-mock-keychain',
+    ]
+
+
     const browserInstance = await puppeteer.launch({
         ignoreHTTPSErrors: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--no-zygote']
+        args: args || minimal_args /* ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--no-zygote']*/
     })
     const page = await browserInstance.newPage()
 
@@ -200,7 +241,7 @@ const openInBrowser = async (options, scope, resolvedData) => {
 
                         await page.screenshot({...pipe.screenshot, path: upload_dir + name});
                         if (!data.screenshot) data.screenshot = []
-                        data.screenshot.push(UPLOAD_URL + '/screenshots/' + name)
+                        data.screenshot.push({...pipe.screenshot,src: UPLOAD_URL + '/screenshots/' + name})
                     }
 
                 } else if (pipe.extract) {
@@ -231,7 +272,12 @@ const openInBrowser = async (options, scope, resolvedData) => {
 
                 } else if (pipe.click) {
                     const btn = await page.$(pipe.click)
-                    await page.evaluate(btn => btn.click(), btn)
+                    if(btn){
+                        await page.evaluate(btn => btn.click(), btn)
+                    } else {
+                        error = `Selector ${pipe.click} for click not exists`
+                        break
+                    }
 
                 } else if (pipe.waitForNavigation) {
                     await page.waitForNavigation(pipe.waitForNavigation)
