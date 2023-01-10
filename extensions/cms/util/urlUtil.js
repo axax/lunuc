c19acb39335a73619558e68899e6682954176c74
Simon Schärer <axax@gmx.net>
2023-01-10 21:44:52 +0100

    Improve scrollByHash function
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- extensions/cms/util/urlUtil.js
+++ extensions/cms/util/urlUtil.js
@@ -1,9 +1,11 @@
 import DomUtil from '../../../client/util/dom.mjs'
+import Util from "../../../client/util/index.mjs";
 
-export const scrollByHash = (url, {scrollStep, scrollOffset, scrollTimeout}) => {
+export const scrollByHash = (url, options) => {
     if (url.indexOf('#') >= 0 && url.length>1) {
          const checkScroll = (el) => {
             if (el) {
+                let {scrollStep, scrollOffset, scrollTimeout} = options
                 const win = window,
                     scrollY = win.scrollY,
                     docEl = win.document.documentElement
@@ -41,8 +43,9 @@ export const scrollByHash = (url, {scrollStep, scrollOffset, scrollTimeout}) => 
                 }
             }
         }
-        DomUtil.waitForElement('#' + url.split('#')[1]).then((el) => {
+        DomUtil.waitForElement('#' + decodeURI(url.split('#')[1])).then((el) => {
             // check until postion is reached
+            options = Object.assign({},_app_.scrollOptions,Util.removeNullValues(options,{removeUndefined:true}))
             checkScroll(el)
         }).catch(()=>{})
     }
