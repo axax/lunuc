1331d32d9d6c007b8397ff8df75bb9ce354fa429
Simon Schärer <axax@gmx.net>
2023-03-14 10:08:46 +0100

    Add option to hide domain in loginContainer
    Signed-off-by: Simon Schärer <axax@gmx.net>

---
+++ extensions/post/translations/admin.js
@@ -0,0 +1,10 @@
+export const translations = {
+    de: {
+        'Post.enterTitle':  'Titel eingeben',
+        'Post.addPost': 'hinzufügen'
+    },
+    en: {
+        'Post.enterTitle':  'Enter a titel',
+        'Post.addPost': 'add'
+    }
+}
