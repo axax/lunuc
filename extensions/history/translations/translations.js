f7f3d77a92c688d599283e99d8f7dc83a04c0ee0
Simon Schärer <axax@gmx.net>
2023-05-13 16:35:32 +0200

    Add translations
    Signed-off-by: Simon Schärer <axax@gmx.net>

---
+++ extensions/history/translations/translations.js
@@ -0,0 +1,10 @@
+export const translations = {
+    de: {
+        'History.field.action': 'Aktion',
+        'History.field.type': 'Type'
+    },
+    en: {
+        'History.field.action': 'Action',
+        'History.field.type': 'Type'
+    }
+}
