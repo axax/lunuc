0221f297d359e41bd08812d8dbe021882100963e
Simon Schärer <axax@gmx.net>
2023-06-11 11:14:35 +0200

    DNS Server
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- extensions/dns/server.mjs
+++ extensions/dns/server.mjs
@@ -1,14 +1,20 @@
+import dns2 from 'dns2'
 import Hook from '../../util/hook.cjs'
-import dns from 'native-dnssec-dns'
-import {consts} from 'native-dnssec-dns-packet'
 import schemaGen from './gensrc/schema'
 import resolverGen from './gensrc/resolver'
 import {deepMergeToFirst} from 'util/deepMerge.mjs'
 import Util from '../../api/util/index.mjs'
-let server = null
-let database = null
-let hosts = {}
-let dbBuffer = {}
+
+
+dns2.Packet.TYPE['HTTPS'] = 65
+const dnsServerContext = {
+    server: false,
+    database: false,
+    hosts: {},
+    dbBuffer: {},
+    settings: {},
+    typeMap: Object.keys(dns2.Packet.TYPE).reduce((a,k)=>{a[dns2.Packet.TYPE[k]]=k;return a},{})
+}
 
 
 // Hook to add mongodb resolver
@@ -21,169 +27,133 @@ Hook.on('schema', ({schemas}) => {
     schemas.push(schemaGen)
 })
 
+
+
 // Hook when db is ready
 Hook.on('appready', async ({db, context}) => {
 
-    let dnsSettings = (await Util.getKeyValueGlobal(db, context, "DnsSettings", true)) || {}
+    dnsServerContext.settings = (await Util.getKeyValueGlobal(db, context, "DnsSettings", true)) || {}
 
-    console.log("DnsSettings", dnsSettings)
-    if (true || !dnsSettings.execfilter || Util.execFilter(dnsSettings.execfilter)) {
+    if (!dnsServerContext.settings.execfilter || Util.execFilter(dnsServerContext.settings.execfilter)) {
 
+
+        // refresh settings every minute
         setInterval(async ()=>{
-            dnsSettings =  (await Util.getKeyValueGlobal(db, context, "DnsSettings", true)) || {}
+            dnsServerContext.settings =  (await Util.getKeyValueGlobal(db, context, "DnsSettings", true)) || {}
         }, 1000 * 60)
 
-        database = db
+        dnsServerContext.database = db
         readHosts(db)
 
 
         console.log('DNS: create dns server')
-        server = dns.createServer()
 
-        server.on('request', (req, res) => {
-            const hostname = req.question[0].name.slice(0, -1),
-                type = req.question[0].type,
-                startTime = new Date().getTime()
+        dnsServerContext.server = dns2.createServer({
+            udp: true,
+            handle: async (request, send, rinfo) => {
+                const response = dns2.Packet.createResponseFromRequest(request)
+                const [ question ] = request.questions
+                const { name } = question
+                const startTime = new Date().getTime()
 
-            if (hosts[hostname] === undefined) {
-                hosts[hostname] = {block: false, subdomains:false}
-            }
-
-            if(!hosts[hostname].count){
-                hosts[hostname].count=0
-            }
-            hosts[hostname].count++
-
-            let block = hosts[hostname].block === true
-
-            if (!block) {
-                //check subdomains
-                let subname = hostname
-                let pos = subname.indexOf('.')
-                while (pos >= 0) {
-                    subname = subname.substring(pos + 1)
-                    if (hosts[subname] && hosts[subname].block === true && hosts[subname].subdomains === true) {
-                        block = true
-                        break
-                    }
-                    pos = subname.indexOf('.')
+                if (dnsServerContext.hosts[name] === undefined) {
+                    dnsServerContext.hosts[name] = {block: false, subdomains:false}
                 }
-            }
 
+                if(!dnsServerContext.hosts[name].count){
+                    dnsServerContext.hosts[name].count=0
+                }
+                dnsServerContext.hosts[name].count++
 
-            if (block && !dnsSettings.disabled) {
-                console.log(`DNS: block host ${hostname} type ${consts.qtypeToName(type)}`)
-                res.answer.push(dns.A({
-                    name: hostname,
-                    address: '0.0.0.0',
-                    ttl: 1,
-                }))
-                res.send()
-            } else {
+                const hostBlocked = isHostBlocked(name)
 
-                console.log(`DNS: ${req._socket._remote.address}:${req._socket._remote.port} resolve host ${hostname} type ${consts.qtypeToName(type)}`)
-                const dnsRequest = dns.Request({
-                    question: req.question[0],
-                    server: {address: '1.1.1.1', port: 53, type: 'udp'},
-                    timeout: 1500
-                })
-
-                dnsRequest.on('timeout', () => {
-                    console.log('DNS: Timeout in making request')
-                })
-
-                dnsRequest.on('message', (err, answer) => {
-                    res.header = Object.assign({},answer.header,{id:res.header.id})
-                    res.question = answer.question
-                    res.answer = answer.answer
-                    res.authority = answer.authority
-                    res.additional = answer.additional
-                    res.edns_options = answer.edns_options
-                    res.payload = answer.payload
-                    res._socket.base_size = 4096
-
-                    res.authority.forEach(authority=>{
-                        // because of node_modules/native-dnssec-dns-packet/packet.js line 425 buff.writeUInt32BE
-                        if(authority.serial>2147483647) {
-                            authority.serial = 999
-                        }
+                if(hostBlocked && !dnsServerContext.settings.disabled){
+                    console.log(`DNS: block ${name}`)
+                    response.answers.push({
+                        name,
+                        type: question.type,
+                        class: question.class,
+                        ttl: 300,
+                        address: '0.0.0.0'
                     })
-                    console.log(`DNS: answer after ${new Date().getTime() - startTime}ms`, answer.answer.map(a=>a.address || a.name).join(' '))
+                    send(response)
 
+                }else {
+                    const resolvedQuestion = await resolveDnsQuestion(question)
+                    response.header = Object.assign({}, resolvedQuestion.header, {id: response.header.id})
+                    response.authorities = resolvedQuestion.authorities
+                    response.answers = resolvedQuestion.answers
+                    response.additionals = resolvedQuestion.additionals
+                    send(response)
 
-                    /*if(answer.authority.length>0){
-                        console.log('DNS: authority', answer.authority.map(a=>a.name).join(' '))
+                    console.log(`DNS: resolved ${name} after ${new Date().getTime()-startTime}ms`)
+                }
+
+                dnsServerContext.dbBuffer[name] = {
+                    updateOne: {
+                        filter: { name },
+                        update: {
+                            $set: {
+                                lastIp: rinfo.address,
+                                lastUsed: new Date().getTime(),
+                                name: name,
+                                count: dnsServerContext.hosts[name].count
+                            }
+                        },
+                        upsert: true
                     }
-                    if(answer.additional.length>0){
-                        console.log('DNS: additional', answer.additional.map(a=>a.name).join(' '))
-                    }*/
-                })
+                }
 
-                dnsRequest.on('end', () => {
-                    try {
-                        res.send()
-
-                        //req._socket.close()
-                    }catch (e){
-                        console.log(e)
-                    }
-                })
-                dnsRequest.send()
-            }
-            dbBuffer[hostname] = {
-                updateOne: {
-                    filter: { name: hostname },
-                    update: {
-                        $set: {
-                            lastIp: req._socket._remote.address,
-                            lastUsed: new Date().getTime(),
-                            name: hostname,
-                            count: hosts[hostname].count
-                        }
-                    },
-                    upsert: true
+                if( Object.keys(dnsServerContext.dbBuffer).length > 20) {
+                    insertBuffer()
                 }
             }
-
-            if( Object.keys(dbBuffer).length > 20) {
-                insertBuffer()
-            }
-        })
-
-        server.on('error', (err, buff, req, res) => {
-            console.log(`DNS: error`,err.stack)
-        })
-
-        server.on('socketError', (err) => {
-            console.log(`DNS: socketError`,err)
         })
 
 
-        server.on('listening', () => {
-            console.log('DNS: listening')
+        dnsServerContext.server.on('request', (request, response, rinfo) => {
+       //     console.log(request.header.id, request.questions[0]);
         })
 
-        server.on('close', () => {
-            console.log('DNS: close')
-        })
-        server.on('socketError', (e) => {
-            console.log('DNS: socketError',e)
+        dnsServerContext.server.on('requestError', (error) => {
+            console.log('DNS: Client sent an invalid request', error)
         })
 
-        server.serve(53)
+        dnsServerContext.server.on('listening', () => {
+            console.log('DNS: listening', dnsServerContext.server.addresses())
+        })
+
+        dnsServerContext.server.on('close', () => {
+            console.log('DNS: server closed')
+        })
+
+        dnsServerContext.server.listen({
+            // Optionally specify port, address and/or the family of socket() for udp server:
+            udp: {
+                port: 53,
+                address: '0.0.0.0',
+                type: 'udp4',  // IPv4 or IPv6 (Must be either "udp4" or "udp6")
+            },
+            // Optionally specify port and/or address for tcp server:
+           /* tcp: {
+                port: 53,
+                address: "0.0.0.0",
+            },*/
+        })
+
+        // eventually
+        // server.close();
     }
-
-
 })
 
 // Hook when the type CronJob has changed
 Hook.on('typeUpdated_DnsHost', ({result}) => {
-    if (result.name && hosts[result.name]) {
+    if (result.name && dnsServerContext.hosts[result.name]) {
         if( result.block !== undefined) {
-            hosts[result.name].block = result.block
+            dnsServerContext.hosts[result.name].block = result.block
         }
         if( result.subdomains !== undefined) {
-            hosts[result.name].subdomains = result.subdomains
+            dnsServerContext.hosts[result.name].subdomains = result.subdomains
         }
     }
 })
@@ -193,19 +163,47 @@ Hook.on('appexit', async () => {
 })
 
 
+const resolveDnsQuestion = async (question) => {
+    const dns = new dns2({
+        dns: dnsServerContext.settings.dns || '1.1.1.1'
+    })
+    const typeName = dnsServerContext.typeMap[question.type]
+
+    const result = await dns.resolve(question.name, typeName, question.class)
+    return result
+}
+
 const readHosts = async (db) => {
     (await db.collection('DnsHost').find().forEach(o => {
-        hosts[o.name] = {block: o.block, subdomains: o.subdomains, count: o.count}
+        dnsServerContext.hosts[o.name] = {block: o.block, subdomains: o.subdomains, count: o.count}
     }))
 }
 
 
-const insertBuffer= async () => {
-    if (database) {
-        console.log('DNS: insert buffer')
+const isHostBlocked = (hostname) => {
+    let block = dnsServerContext.hosts[hostname].block === true
 
-        await database.collection('DnsHost').bulkWrite(Object.values(dbBuffer), { ordered: false })
-        dbBuffer = {}
+    if (!block) {
+        //check subdomains
+        let subname = hostname
+        let pos = subname.indexOf('.')
+        while (pos >= 0) {
+            subname = subname.substring(pos + 1)
+            if (dnsServerContext.hosts[subname] && dnsServerContext.hosts[subname].block === true && dnsServerContext.hosts[subname].subdomains === true) {
+                block = true
+                break
+            }
+            pos = subname.indexOf('.')
+        }
+    }
+    return block
+}
+
+const insertBuffer= async () => {
+    if (dnsServerContext.database) {
+        console.log('DNS: insert buffer')
+        await dnsServerContext.database.collection('DnsHost').bulkWrite(Object.values(dnsServerContext.dbBuffer), { ordered: false })
+        dnsServerContext.dbBuffer = {}
     }
 }
 
