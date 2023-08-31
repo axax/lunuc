ea01b9a952f88aadc90dc2ae138cc0f3d8ba262b
Simon Schärer <axax@gmx.net>
2023-08-31 06:39:38 +0200

    Add set initial password functionality
    Signed-off-by: Simon Schärer <axax@gmx.net>

---
+++ util/password.mjs
@@ -0,0 +1,10 @@
+export const generatePassword = () => {
+    let pass = ''
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$'
+
+    for (let i = 1; i <= 8; i++) {
+        let char = Math.floor(Math.random()* str.length + 1)
+        pass += str.charAt(char)
+    }
+    return pass
+}
