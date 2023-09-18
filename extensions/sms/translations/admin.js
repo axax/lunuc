8e7783db7f7182635e51b4b5bb62c8d738d0d7b7
Simon Schärer <axax@gmx.net>
2023-09-18 21:43:07 +0200

    Add ownerGroup to SmsLog
    Signed-off-by: Simon Schärer <axax@gmx.net>

---
+++ extensions/sms/translations/admin.js
@@ -0,0 +1,18 @@
+export const translations = {
+    de: {
+        'SmsLog.field.content': 'Inhalt',
+        'SmsLog.field.sender': 'Absender',
+        'SmsLog.field.recipient': 'Empfänger',
+        'SmsLog.field.domain': 'Domäne',
+        'SmsLog.field.response': 'Serverantwort',
+        'SmsLog.field.ownerGroup': 'Gruppe'
+    },
+    en: {
+        'SmsLog.field.content': 'Content',
+        'SmsLog.field.sender': 'Sender',
+        'SmsLog.field.recipient': 'Recipient',
+        'SmsLog.field.domain': 'Domain',
+        'SmsLog.field.response': 'Server Response',
+        'SmsLog.field.ownerGroup': 'Group'
+    }
+}
