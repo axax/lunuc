969df500514337eabb78db896b7ad51bbf73ff3c
Simon Schärer <axax@gmx.net>
2024-02-29 22:57:40 +0100

    Temp log
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- extensions/mailserver/smtp/index.mjs
+++ extensions/mailserver/smtp/index.mjs
@@ -155,6 +155,9 @@ const startListening = async (db, context) => {
                     })
 
                     const transporterResult = nodemailer.createTransport(transporter)
+
+                    delete data.to
+                    delete data.from
                     console.log('xxxx',data)
 
                   //  for(const rcpt of session.envelope.rcptTo){
