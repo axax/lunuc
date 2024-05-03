da571b7679921bd466fd26b90a2e82a9dd73b070
Simon Schärer <axax@gmx.net>
2024-05-03 23:06:45 +0200

    Small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- shell/start.sh
+++ shell/start.sh
@@ -16,18 +16,18 @@ npm run build
 
 echo "restart "$restart
 
-if [ $restart -eq "all" ]
+if [ $restart = "all" ]
 then
   sudo systemctl restart lunuc-client
   sudo systemctl restart lunuc-api
 fi
 
-if [ $restart -eq "client" ]
+if [ $restart = "client" ]
 then
   sudo systemctl restart lunuc-client
 fi
 
-if [ $restart -eq "api" ]
+if [ $restart = "api" ]
 then
   sudo systemctl restart lunuc-api
 fi
