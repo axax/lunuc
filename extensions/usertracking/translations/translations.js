0b06ba717577f87191e324bfe24bf537fcabec06
Simon Schärer <axax@gmx.net>
2024-12-16 21:46:40 +0100

    small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

---
+++ extensions/usertracking/translations/translations.js
@@ -0,0 +1,36 @@
+export const translations = {
+    de: {
+        'UserTracking.field.ip': 'IP Adresse',
+        'UserTracking.field.agent': 'User-Agent',
+        'UserTracking.field.host': 'Host',
+        'UserTracking.field.slug': 'CMS Seite',
+        'UserTracking.field.path': 'Url Pfad',
+        'UserTracking.field.day': 'Tag',
+        'UserTracking.field.month': 'Monat',
+        'UserTracking.field.year': 'Jahr',
+        'UserTracking.field.event': 'Event',
+        'UserTracking.field.isBot': 'Bot Aufruf',
+        'UserTracking.field.data': 'Daten',
+        'UserTracking.field.headers': 'HTTP Header',
+        'UserTracking.field.referer': 'Referer',
+        'UserTracking.field.geodata': 'Geo',
+        'UserTracking.field.server': 'Server'
+    },
+    en: {
+        'UserTracking.field.ip': 'IP Address',
+        'UserTracking.field.agent': 'User-Agent',
+        'UserTracking.field.host': 'Host',
+        'UserTracking.field.slug': 'CMS page',
+        'UserTracking.field.path': 'Url path',
+        'UserTracking.field.day': 'Day',
+        'UserTracking.field.month': 'Month',
+        'UserTracking.field.year': 'Year',
+        'UserTracking.field.event': 'Event',
+        'UserTracking.field.isBot': 'Is bot',
+        'UserTracking.field.data': 'Data',
+        'UserTracking.field.headers': 'HTTP Header',
+        'UserTracking.field.referer': 'Referer',
+        'UserTracking.field.geodata': 'Geo',
+        'UserTracking.field.server': 'Server'
+    }
+}
