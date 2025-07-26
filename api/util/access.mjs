dcac4dbda12d31e2f18bddcfcf9126e19668cbf2
Simon Schärer <axax@gmx.net>
2025-07-26 20:12:17 +0200

    small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

---
+++ api/util/access.mjs
@@ -0,0 +1,19 @@
+
+export const userHasAccessToObject =(context, item) =>{
+    const createdByIdAsString = item.createdBy?item.createdBy.toString():''
+    if(createdByIdAsString === context.id){
+        return true
+    }
+    if(context.junior && context.junior.indexOf(createdByIdAsString)>=0){
+        return true
+    }
+    if(item?.ownerGroup?.length>0 && context?.group?.length>0){
+        for(const ownerGroup of item.ownerGroup){
+            const ownerGroupAsString = ownerGroup.toString()
+            if(context.group.indexOf(ownerGroupAsString)>=0){
+                return true
+            }
+        }
+    }
+    return false
+}
