007a83dbe160f96c22ef17ecac9da5e2ec2dd7b8
Simon Schärer <axax@gmx.net>
2025-11-11 23:05:25 +0100

    small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- api/util/jwt.mjs
+++ api/util/jwt.mjs
@@ -5,22 +5,18 @@ import {AUTH_SCHEME,SECRET_KEY} from '../constants/index.mjs'
 export const decodeToken = (token) => {
     let result = {}
     if (token) {
-        try {
-            const matches = token.match(/(\S+)\s+(\S+)/)
-            if (matches && matches.length > 1 && matches[1] === AUTH_SCHEME) {
-                // verify a token symmetric - synchronous
-                jwt.verify(matches[2], SECRET_KEY, (err, decoded) => {
-                    if (!err) {
-                        result = decoded
-                    } else {
-                        console.error('decodeToken verify error', err)
-                    }
-                })
-            }
-            result.auth = token
-        }catch (e){
-            console.error('decodeToken error', err)
+        const matches = token.match(/(\S+)\s+(\S+)/)
+        if (matches && matches.length > 1 && matches[1] === AUTH_SCHEME) {
+            // verify a token symmetric - synchronous
+            jwt.verify(matches[2], SECRET_KEY, (err, decoded) => {
+                if (!err) {
+                    result = decoded
+                } else {
+                    console.error('decodeToken verify error', err)
+                }
+            })
         }
+        result.auth = token
     }
     return result
 }
