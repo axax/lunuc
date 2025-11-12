2aea94d50e435afcd64e53e90c594707102e8f8b
Simon Schärer <axax@gmx.net>
2025-11-12 11:44:08 +0100

    small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- extensions/mailserver/imap/index.mjs
+++ extensions/mailserver/imap/index.mjs
@@ -733,7 +733,7 @@ const startListening = async (db, context) => {
                         subject: message.data.subject,
                         text: message.data.text,
                         html: message.data.html,
-                        date: new Date(message.data.date || Util.formattedDateFromObjectId(message._id.toString())).toUTCString(), // important for preserving sent date
+                        date: new Date(message.data.date || Util.dateFromObjectId(message._id.toString(), new Date())).toUTCString(), // important for preserving sent date
                        // alternatives: message.data.alternatives,
                         attachments: Array.isArray(message.data.attachments) ? message.data.attachments.map(att => ({
                             filename: att.filename,
@@ -743,6 +743,10 @@ const startListening = async (db, context) => {
                             encoding: att.encoding,
                             contentDisposition: att.contentDisposition,
                         })): [],
+                    }
+
+                    if(messageData.date==='Invalid Date'){
+                        messageData.date = new Date().toUTCString()
                     }
 
                     try {
