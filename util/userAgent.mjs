b8733417acef56812de4f22370ecbb8bb94c359b
Simon Schärer <axax@gmx.net>
2026-02-23 23:42:16 +0100

    small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- util/userAgent.mjs
+++ util/userAgent.mjs
@@ -217,7 +217,7 @@ export const parseUserAgentRaw = (() => {
 })()
 
 export const isLetsEncryptAgent = (agent) => {
-    return agent.indexOf('www.letsencrypt.org') >= 0
+    return agent && agent.indexOf('www.letsencrypt.org') >= 0
 }
 
 //console.log(parseUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'))
