9f05b6bfbf4ef03a8130e4d6c9aa3b3dae6a2ed1
Simon Schärer <axax@gmx.net>
2026-09-14 17:39:36 +0200

    small improvements
    Signed-off-by: Simon Schärer <axax@gmx.net>

--- extensions/newsletter/resolver/index.mjs
+++ extensions/newsletter/resolver/index.mjs
@@ -207,21 +207,21 @@ export default db => ({
                         }
                     }
 
-                    if(finalSubject.constructor === Object){
+                    if(finalSubject instanceof Object){
                         if(finalSubject[subLang]){
                             finalSubject = finalSubject[subLang]
                         }else{
                             finalSubject = finalSubject[config.DEFAULT_LANGUAGE]
                         }
                     }
-                    if(finalText.constructor === Object){
+                    if(finalText instanceof Object){
                         if(finalText[subLang]){
                             finalText = finalText[subLang]
                         }else{
                             finalText = finalText[config.DEFAULT_LANGUAGE]
                         }
                     }
-                    if(finalHtml && finalHtml.constructor === Object){
+                    if(finalHtml && finalHtml instanceof Object){
                         if(finalHtml[subLang]){
                             finalHtml = finalHtml[subLang]
                         }else{
